6fe67ddbe037dc8888ef1efa27345e2465e9618a
Bahtiar Ariyanki <bahtiarariyanki@Bahtiars-MacBook-Air.local>
2023-10-10 18:29:29 +0700

    init

---
+++ systems/automap/authmap.go
@@ -0,0 +1,17 @@
+package automap
+
+import "encoding/json"
+
+func AutoMap(source interface{}, destination interface{}) error {
+	sourceJSON, err := json.Marshal(source)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(sourceJSON, destination)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
