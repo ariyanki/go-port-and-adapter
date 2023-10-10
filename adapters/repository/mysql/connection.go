1356c0a1862f13175aa0c5d157ee454e26d63389
Bahtiar Ariyanki <bahtiarariyanki@Bahtiars-MacBook-Air.local>
2023-10-10 15:39:47 +0700

    init

--- adapters/repository/mysql/connection.go
+++ adapters/repository/mysql/connection.go
@@ -13,63 +13,27 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-//Database Database
-type Database struct {
-	Host      string
-	User      string
-	Password  string
-	DBName    string
-	DBNumber  int
-	Port      int
-	Pool 	  DbPool
-	DebugMode bool
-}
-
-type DbPool struct {
-	MaxOpenConns  int
-	MaxIdleConns  int
-	MaxLifetime  time.Duration
-}
-
 func init() {
 	config.Load()
 }
 
-// LoadDBConfig load database configuration
-func LoadDBConfig(name string) Database {
-	db := viper.Sub("database." + name)
-	conf := Database{
-		Host:      db.GetString("host"),
-		User:      db.GetString("user"),
-		Password:  db.GetString("password"),
-		DBName:    db.GetString("db_name"),
-		Port:      db.GetInt("port"),
-		Pool: 	   DbPool{
-			MaxOpenConns:  db.GetInt("maxOpenConns"),
-			MaxIdleConns:  db.GetInt("maxIdleConns"),
-			MaxLifetime:  db.GetDuration("maxLifetime"),
-		},
-		DebugMode: db.GetBool("debug"),
-	}
-	return conf
-}
-
 // MysqlConnect connect to mysql using config name. return *gorm.DB incstance
 func MysqlConnect(configName string) *gorm.DB {
-	mysql := LoadDBConfig(configName)
-	connection, err := gorm.Open("mysql", mysql.User+":"+mysql.Password+"@tcp("+mysql.Host+":"+strconv.Itoa(mysql.Port)+")/"+mysql.DBName+"?charset=utf8&parseTime=True&loc=Local")
+	mysql := viper.Sub("database." + configName)
+	connection, err := gorm.Open("mysql", mysql.GetString("user")+":"+mysql.GetString("password")+"@tcp("+mysql.GetString("host")+":"+strconv.Itoa(mysql.GetInt("host"))+")/"+mysql.GetString("db_name")+"?charset=utf8&parseTime=True&loc=Local")
 	// enable debug
 	if err != nil {
 		panic(err)
 	}
 
-	if mysql.DebugMode {
+	if mysql.GetBool("debug") {
 		return connection.Debug()
 	}
 
-	connection.DB().SetMaxOpenConns(mysql.Pool.MaxOpenConns)
-	connection.DB().SetMaxIdleConns(mysql.Pool.MaxIdleConns)
-	connection.DB().SetConnMaxLifetime(mysql.Pool.MaxLifetime * time.Second)
+	pool := viper.Sub("database." + configName + ".pool")
+	connection.DB().SetMaxOpenConns(pool.GetInt("maxOpenConns"))
+	connection.DB().SetMaxIdleConns(pool.GetInt("maxIdleConns"))
+	connection.DB().SetConnMaxLifetime(pool.GetDuration("maxLifetime") * time.Second)
 
 	return connection
 }
